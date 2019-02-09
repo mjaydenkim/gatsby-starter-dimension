8763088f03c25dd58eecf69999a4dfbba7baab7b
mjaydenkim <mjaydenkim@gmail.com>
2019-02-08 17:14:38 -0800

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -18,7 +18,7 @@ class Main extends React.Component {
           <span className="image main"><img src={pic01} alt="" /></span>
           <h3>Name:</h3>
           <p>Osmium</p>
-          <h3>Periodic Square:</h3>
+          <p>Periodic Square:</p>
           <img src="https://i.imgur.com/UPxmcjj.jpg" alt="Periodic Table"/>
           {close}
         </article>
